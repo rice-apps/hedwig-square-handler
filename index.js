22404b417d0dbd9f9c9a126a99973f1ee80155de
Victor Song <vms2@rice.edu>
2020-08-05 01:06:28 -0500

    Added untested code for requesting new access token

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { OAuthApi, ObtainTokenRequest } from "square-connect";
+import jwt from "jsonwebtoken";
+import moment from "moment";
 
 let apiInstance = new OAuthApi();
 
@@ -14,14 +16,14 @@ async function handleRequest(request) {
     let response;
     if (request.method === "POST") {
         const { accessCode } = await request.json();
-        const body = new ObtainTokenRequest();
 
         const {
             merchant_id,
             refresh_token,
             access_token,
+            expires_at,
         } = await apiInstance.obtainToken({
-            ...body,
+            ...new ObtainTokenRequest(),
             client_id: SQUARE_APP_ID,
             client_secret: SQUARE_APP_SECRET,
             grant_type: "authorization_code",
@@ -29,11 +31,49 @@ async function handleRequest(request) {
         });
 
         if (refresh_token) {
-            await AUTH.put(merchant_id, { refresh_token, access_token });
+            await AUTH.put(merchant_id, {
+                refresh_token,
+                access_token,
+                expires_at,
+            }); // TODO: create new KV namespace called AUTH
 
             response = new Response("success", { status: 200 });
         } else {
             response = new Response("failure", { status: 404 });
+        }
+    } else if (request.method === "GET") {
+        try {
+            jwt.verify(request.headers.authorization, ACCESS_SECRET); // TODO: bind ACCESS_SECRET to this worker
+
+            const { merchant } = await request.json();
+
+            const { refresh_token, expires_at } = await AUTH.get(merchant);
+
+            if (moment().isAfter(expires_at)) {
+                const {
+                    merchant_id,
+                    access_token,
+                    refresh_token: same_refresh_token,
+                    expires_at: new_expires_at,
+                } = await apiInstance.obtainToken({
+                    ...new ObtainTokenRequest(),
+                    grant_type: "refresh_token",
+                    refresh_token,
+                });
+
+                await AUTH.delete(merchant_id);
+                await AUTH.put(merchant_id, {
+                    same_refresh_token,
+                    access_token,
+                    expires_at: new_expires_at,
+                });
+            }
+
+            return new Response(await AUTH.get(merchant).access_token, {
+                status: 200,
+            });
+        } catch (err) {
+            return new Response("Auth error", { status: 403 });
         }
     } else {
         response = new Response("Expected POST", { status: 405 });
